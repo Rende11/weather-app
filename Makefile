cffe75d1f5850e6f0a1807b5a33b9b7cc101baef
Rende11 <artamonovn@gmail.com>
2022-05-24 23:07:25 +0300

    Add app run entrypoint.

--- Makefile
+++ Makefile
@@ -12,3 +12,6 @@ down:
 
 psql:
 		psql -h $(POSTGRES_HOST) -p 5433 -U $(POSTGRES_USER) -d $(POSTGRES_DB)
+
+run:
+		clj -M:run
--- src/weather_app/core.clj
+++ src/weather_app/core.clj
@@ -1,1 +1,5 @@
-(ns weather-app.core)
+(ns weather-app.core
+  (:require [weather-app.system :as system]))
+
+(defn -main [& args]
+  (system/start))
