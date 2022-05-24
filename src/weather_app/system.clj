adc796e24b5ad9a9795a3f5127a20a126f91b052
Rende11 <artamonovn@gmail.com>
2022-05-25 00:38:59 +0300

    Use aero for app configuration.

--- src/weather_app/system.clj
+++ src/weather_app/system.clj
@@ -1,46 +1,41 @@
 (ns weather-app.system
-  (:require [hikari-cp.core :as cp]
+  (:require [aero.core :refer (read-config)]
+            [clojure.java.io :as io]
+            [hikari-cp.core :as cp]
             [integrant.core :as ig]
             [ring.adapter.jetty :as jetty]
             [weather-app.handler :as handler]))
 
-(def config
-  {::server  {:handler (ig/ref ::handler)
-              :port    (or (some-> (System/getenv "HTTP_PORT")
-                                   (Integer/parseInt))
-                           8080)}
-   ::handler {:api-key (System/getenv "API_KEY")
-              :db      (ig/ref ::db)}
-   ::db      {:adapter       "postgresql"
-              :database-name (System/getenv "POSTGRES_DB")
-              :server-name   (System/getenv "POSTGRES_HOST")
-              :username      (System/getenv "POSTGRES_USER")
-              :password      (System/getenv "POSTGRES_PASSWORD")
-              :port-number   (System/getenv "POSTGRES_PORT")}})
 
-(defmethod ig/init-key ::server [_ {:keys [handler port]}]
+(defmethod aero.core/reader 'ig/ref
+  [_opts _tag value]
+  (integrant.core/ref value))
+
+(defmethod ig/init-key :app/server [_ {:keys [handler port]}]
   (prn (str "Server is launching on port: " port))
   (jetty/run-jetty handler {:port port :join? false}))
 
-(defmethod ig/init-key ::handler [_ {:keys [api-key db]}]
+(defmethod ig/init-key :app/handler [_ {:keys [api-key db]}]
   (fn [req]
-    (handler/app-handler (assoc req :ctx {:api-key api-key
-                                          :db db}))))
+    (handler/app-handler (assoc req :ctx {:api-key api-key :db db}))))
 
-(defmethod ig/init-key ::db [_ config]
+(defmethod ig/init-key :app/db [_ config]
   {:datasource (cp/make-datasource config)})
 
 
-(defmethod ig/halt-key! ::server [_ server]
+(defmethod ig/halt-key! :app/server [_ server]
   (.stop server))
 
-(defmethod ig/halt-key! ::db [_ db-opts]
+(defmethod ig/halt-key! :app/db [_ db-opts]
   (-> db-opts
       :datasource
       cp/close-datasource))
 
+
 (defn start []
-  (ig/init config))
+  (-> (io/resource "config.edn")
+      read-config
+      ig/init))
 
 (defn stop [sys]
   (ig/halt! sys))
